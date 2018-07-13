16be0eddcb6934896787ffb3a2ce65b7d3c42c0c
a012001734 <a012001734@H02640A01.HO.AD.BDO>
2018-07-13 13:06:20 +0800

    - Used Spring Batch Metadata in memory

--- src/main/java/com/ef/service/batch/configuration/BatchConfiguration.java
+++ src/main/java/com/ef/service/batch/configuration/BatchConfiguration.java
@@ -12,18 +12,24 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  *
@@ -42,7 +48,26 @@ public class BatchConfiguration {
     
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
-    
+
+    // -------------------------------------------------------------------------
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new ResourcelessTransactionManager();
+    }
+
+    @Bean
+    public JobRepository jobRepository() throws Exception {
+        return new MapJobRepositoryFactoryBean(transactionManager()).getObject();
+    }
+
+    @Bean
+    public JobLauncher jobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository());
+        return jobLauncher;
+    }
+    // -------------------------------------------------------------------------
+
     @Autowired
     private AccessLogItemProcessor processor;
     
